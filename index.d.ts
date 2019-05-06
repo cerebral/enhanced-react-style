ebb5bf769b33f4be8077107142b94dc472340637
FWeinb <fabrice@weinberg.me>
2019-05-06 19:58:37 +0200

    add ts types

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+declare module 'react' {
+  interface CSSProperties {
+    '&'?: {
+      [selector: string]: Omit<CSSProperties, '&'>;
+    };
+  }
+}
